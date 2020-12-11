a6320a3073bc01ecc7f2b729d4eed84ecfe36743
Renan <stdio@MacBook-Pro-de-Renan.local>
2020-12-11 01:30:04 -0300

    habilitando cors

--- src/database/migrations/1606885482156-AlterAppointmentFieldToProviderId.ts
+++ src/database/migrations/1606885482156-AlterAppointmentFieldToProviderId.ts
@@ -1,5 +1,4 @@
 import {
-  Column,
   MigrationInterface,
   QueryRunner,
   TableColumn,
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/upload';
-
 import './database';
 import AppError from './errors/AppError';
 // inicia servidor
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
